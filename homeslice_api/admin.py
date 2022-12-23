2da2504affa73683f571e74717dcf36896c04a21
wolfsup7 <laurendooley@gmail.com>
2022-12-23 12:15:49 -0500

    admin site up and running user-admin pw-admin

--- homeslice_api/admin.py
+++ homeslice_api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Customer
+admin.site.register(Customer)
+from .models import Ingredients
+admin.site.register(Ingredients)
+from .models import Pizza
+admin.site.register(Pizza)
+from .models import Order
+admin.site.register(Order)
