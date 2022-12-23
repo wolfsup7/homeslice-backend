51a22cd2033242fb8fe7854ba3e0b9affa7f621e
wolfsup7 <laurendooley@gmail.com>
2022-12-23 10:54:01 -0500

    models made

--- homeslice_api/models.py
+++ homeslice_api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=50, null=True)
+
+class Ingredients(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    category = models.CharField(max_length=50, null=True)
+
+    def _str_(self):
+        return self.name
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    price = models.FloatField(null=True)
+    image = models.URLField(max_length=200, null=True)
+    creator = models.CharField(max_length=500, null=True)
+    ingredients = models.ManyToManyField(Ingredients)
+
+    def _str_(self):
+        return self.name
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, null=True, on_delete= models.SET_NULL)
+    pizza = models.ForeignKey(Pizza, null=True, on_delete= models.SET_NULL)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
--- homeslice_rest_api/settings.py
+++ homeslice_rest_api/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'homeslice_api',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,10 +76,14 @@ WSGI_APPLICATION = 'homeslice_rest_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'homeslice',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost'
     }
 }
+
 
 
 # Password validation
